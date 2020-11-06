1887ec146cd3936425719c8fad250c00f7cd03e5
Constantin Nickel <constantin.nickel@gmail.com>
2020-11-06 23:03:14 +0100

    main.py

---
+++ .github/actions/toolchain/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os, subprocess
+
+def to_list(s):
+    return [x for x in map(str.strip, s.split(',')) if len(x) > 0]
+
+toolchain = os.environ('INPUT_TOOLCHAIN', 'stable')
+
+targets = os.environ.get('INPUT_TARGETS', '')
+targets = ['-t', *to_list(targets)]
+
+components = os.environ.get('INPUT_COMPONENTS', '')
+components = ['-c', *to_list(components)]
+
+subprocess.run(['rustup', 'self', 'update'])
+subprocess.run(['rustup', 'set', 'profile', 'minimal'])
+
+args = ['rustup', 'toolchain', 'install', toolchain, *targets, *components]
+
+print(args)
